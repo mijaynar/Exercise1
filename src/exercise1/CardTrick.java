e526b24d4f39b75c9f4e5d8c25cbadb912d31c81
Jay Mistry <mijaynar@sheridancollege.ca>
2024-01-31 12:47:00 -0500

    Modified this file on 31/01/2024 //Second Commit

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,6 +1,10 @@
 package exercise1;
 
-/**
+import java.util.Random;
+import java.util.Scanner;
+
+/**@author Jay Nareshbhai Mistry
+ * Date: 31/01/2024
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
@@ -13,15 +17,33 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
-
+        Random random = new Random();
+          
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+            card.setValue(random.nextInt(13) + 1);
+            card.setSuit(Card.SUITS[random.nextInt(4)]);
+            hand[i] = card;
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess and enter any card number");
+        int guessedValue = scanner.nextInt();
+        System.out.println("Enter the suit of the card you want to guess (1 for Hearts, 2 for Diamonds, 3 for Spades, 4 for Clubs):");
+        int guessedSuit = scanner.nextInt();
+
+        for (Card card : hand) {
+            if (card.getValue() == guessedValue && card.getSuit().equals(Card.SUITS[guessedSuit - 1])) {
+                printInfo();
+                break;
+            } 
+        }
+
+        scanner.close();
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
